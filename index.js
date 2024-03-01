0d99577d7e6627f92de23419b453d4336e5c5db4
Liam Vermette <Liam.vermette@gmail.com>
2024-02-29 19:59:21 -0500

    added background instead

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function tile(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
 function newImage(address, Xcoord, Ycoord) {
     let image = document.createElement('img')
     image.src = address
@@ -15,7 +23,12 @@ function newItem(address, Xcoord, Ycoord){
     })
 }
 
+let horizon = window.innerHeight / 2
+let heightSky = window.innerHeight-horizon
+let heightGrass = horizon
 
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightGrass/100)
 
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
