df345add00a6bd89f48010ab8213dd76d72d821b
Liam Vermette <Liam.vermette@gmail.com>
2024-02-29 19:55:20 -0500

    I saw we will add walking later :monocle_face:

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ function newItem(address, Xcoord, Ycoord){
     })
 }
 
-newImage('assets/green-Character.gif', 100, 100)
+
+
+newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
@@ -25,3 +27,4 @@ newImage('assets/well.png', 500, 425)
 newItem('assets/sword.png', 500, 405)
 newItem('assets/shield.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
+
