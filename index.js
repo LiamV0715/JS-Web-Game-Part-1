55f303e0b4eaeb8450588d196f0196e098bd5457
Liam Vermette <Liam.vermette@gmail.com>
2024-02-29 19:37:11 -0500

    adding something fun on top

--- index.js
+++ index.js
@@ -8,11 +8,20 @@ function newImage(address, Xcoord, Ycoord) {
     return image
   }
 
-newImage(green-Character.gif, 100, 100)
+function newItem(address, Xcoord, Ycoord){
+    let image = newImage(address, Xcoord, Ycoord)
+    image.addEventListener('dblclick', () => {
+      image.remove()
+    })
+}
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '400px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+newImage('assets/green-Character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
